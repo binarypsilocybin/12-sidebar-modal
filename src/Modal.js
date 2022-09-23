a37615ddfb288f08501781973032b25aefa66c98
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-09-23 13:45:28 +0100

    1 file modified

--- src/Modal.js
+++ src/Modal.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { useGlobalContext } from './context';
 const Modal = () => {
-  const {}
+  const { isModalOpen, closeModal } = useGlobalContext();
   return (
-    <div className={`modal-overlay`}>
+    <div
+      className={`${
+        isModalOpen ? 'modal-overlay show-model' : 'modal-overlay'
+      }`}
+    >
       <div className="modal-container">
         <h3>modal content</h3>
         <button className="close-modal-btn">
