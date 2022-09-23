defd5826e70d94ffee50e4763bc106fe017de74b
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-09-23 13:16:04 +0100

    3 files modified

--- src/context.js
+++ src/context.js
@@ -1,1 +1,9 @@
 import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="">{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppProvider };
